5eae65313ce842d09e1f9b627f2e770d2bea3a77
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2007-11-22 09:40:37 +0000

    Pure Java

---
+++ Alchemy/src/alchemy/AlcPlugin.java
@@ -0,0 +1,18 @@
+/**
+ * AlcPlugin.java
+ *
+ * Created on November 22, 2007, 6:38 PM
+ *
+ * @author  Karl D.D. Willis
+ * @version 1.0
+ */
+
+package alchemy;
+
+public class AlcPlugin {
+    
+    /** Creates a new instance of AlcPlugin */
+    public AlcPlugin() {
+    }
+    
+}
--- Alchemy/src/alchemy/AlcShape.java
+++ Alchemy/src/alchemy/AlcShape.java
@@ -7,136 +7,155 @@
 
 package alchemy;
 
-import processing.core.PApplet;
-import processing.core.PGraphics;
-
 import java.awt.Point;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import java.awt.geom.GeneralPath;
+
 public class AlcShape {
     
-    // Minimum space between points
-    int space = 5;
-    PApplet root;
-    PGraphics canvas;
+    GeneralPath shape;
     
-    ArrayList<Point> points;
-    int colour;
+    /** Colour of this shape */
+    Color colour = Color.BLACK;
     
-    boolean endLine = false;
-    Point end = new Point();
+    /** Alpha of this shape */
+    int alpha = 255;
     
+    /** Style of this shape - (1) LINE or (2) SOLID FILL */
+    int style = 1;
     
-    public AlcShape(PApplet root, PGraphics canvas, Point pt){
-        init(root, canvas, pt, 0x000000);
+    /** Line Weight if the style is line */
+    int lineWidth = 1;
+    
+    /** Store the last point */
+    Point lastPt;
+    
+    /** For drawing smaller marks - draw lines until x points have been made */
+    int startPoints = 5;
+    
+    /** Minimum distance until points are added */
+    int minMovement = 5;
+    
+    /** Keep track of the number of points added */
+    int totalPoints = 0;
+    
+    public AlcShape(Point p){
+        setup(p, colour, alpha, style, lineWidth);
     }
     
-    public AlcShape(PApplet root, PGraphics canvas, Point pt, int colour){
-        init(root, canvas, pt, colour);
+    public AlcShape(Point p, Color aColour, int aAlpha, int aStyle, int aLineWidth){
+        setup(p, aColour, aAlpha, aStyle, aLineWidth);
     }
     
-    public void init(PApplet root,  PGraphics canvas, Point pt, int c){
-        this.root = root;
-        this.canvas = canvas;
-        points = new ArrayList<Point>(1000);
-        points.ensureCapacity(1000);
-        points.add(pt);
-        this.colour = c;
-    }
-    
-    public void draw(){
+    public void setup(Point p, Color aColour, int aAlpha, int aStyle, int aLineWidth){
         
-        //root.fill(colour);
+        //System.out.println("Setup");
         
+        /** Create the shape and move to the first point */
+        shape = new GeneralPath();
+        shape.moveTo(p.x, p.y);
+        totalPoints++;
+        lastPt = p;
         
-        Point pt1 = (Point)points.get(0);
+        //System.out.println(p.x + " " + p.y);
         
-        //root.beginShape();
-        //root.vertex(pt1.x, pt1.y);
-        
-        for(int i = 1; i < points.size(); i++) {
-            //for(int i = 1; i < points.size(); i++) {
-            //Point ppt = (Point)points.get(i-1);
-            Point ppt = (Point)points.get(i-1);
-            Point pt = (Point)points.get(i);
-            
-            //int movement = root.abs(pt.x - ppt.x) + root.abs(pt.y - ppt.y);
-            //root.println(movement);
-            
-            //if(movement > 25){
-            //root.curveVertex(pt.x, pt.y);
-            root.line(ppt.x, ppt.y, pt.x, pt.y);
-            
-            //} else {
-            // root.vertex(pt.x, pt.y);
-            //}
-        }
-        /*
-        if (endLine){
-            root.vertex(end.x, end.y);
-        }
-         */
-        
-        //root.endShape();
+        alpha = aAlpha;
+        setColour(aColour);
+        style = aStyle;
+        lineWidth = aLineWidth;
         
     }
     
-    public void drag(Point pt) {
-        // Get the last point
-        Point ppt = (Point)points.get(points.size()-1);
+    public void drag(Point p) {
         
-        // If there is movement then add another point
-        int movement = root.abs(pt.x - ppt.x) + root.abs(pt.y - ppt.y);
-        
-        // If drawing a small mark or the start of a mark
-        if(points.size() < space){
+        // At the start just draw lines so smaller marks can be made
+        if(totalPoints < startPoints) {
             
-            // Draw a points if there is movement
-            if(movement > 0){
-                points.add(pt);
+            shape.lineTo(p.x,  p.y);
+            savePoints(p);
+            
+        } else {
+            
+            // Movement since the last point was drawn
+            int movement =  Math.abs(p.x - lastPt.x) + Math.abs(p.y - lastPt.y);
+            
+            // Test to see if this point has moved far enough
+            if(movement > minMovement){
                 
-                /*
-                canvas.beginDraw();
-                canvas.line(ppt.x, ppt.y, pt.x, pt.y);
-                canvas.endDraw();
-                 */
+                // New control point value
+                Point pt = new Point();
+                
+                // Average the points
+                pt.x = (lastPt.x + p.x) >> 1;
+                pt.y = (lastPt.y + p.y) >> 1;
+                
+                //System.out.println("Last Point: " + lastPt.x + " " + lastPt.y + " - Edited Point: " + pt.x + " " + pt.y + " - Original Point: " + p.x + " " + p.y);
+                
+                // Add the Quadratic curve - control point x1, y1 and actual point x2, y2
+                shape.quadTo(lastPt.x, lastPt.y, pt.x,  pt.y);
+                savePoints(p);
+                
             }
-        } else{
-            
-            if(movement > space) {
-                points.add(pt);
-                
-                /*
-                canvas.beginDraw();
-                canvas.line(ppt.x, ppt.y, pt.x, pt.y);
-                canvas.endDraw();
-                 */
-                
-                endLine = false;
-            } else {
-                end = pt;
-                endLine = true;
-            }
-            
         }
     }
     
-    public void release(Point pt) {
-        if(endLine){
-            points.add(pt);
-            //end.x = x;
-            //end.y = y;
-        }
-        root.println(points.size());
+    public void savePoints(Point p){
+        
+        // Increment the total number of points
+        totalPoints++;
+        
+        // Set the current point to the (original) last point value - not the altered pt value
+        lastPt = p;
+        
     }
     
-    public int getColour(){
+    public void release(Point xy) {
+        shape.lineTo(xy.x, xy.y);
+    }
+    
+    
+    // ALCSHAPE Interfaces
+    public GeneralPath getShape(){
+        return shape;
+    }
+    
+    public void setShape(GeneralPath aShape){
+        shape = aShape;
+    }
+    
+    public Color getColour(){
         return colour;
     }
     
-    public void setColour(int c){
-        colour = c;
+    public void setColour(Color c){
+        colour = new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+    }
+    
+    public int getAlpha(){
+        return alpha;
+    }
+    
+    public void setAlpha(int a){
+        alpha = a;
+    }
+    
+    public int getStyle(){
+        return style;
+    }
+    
+    public void setStyle(int s){
+        style = s;
+    }
+    
+    public int getLineWidth(){
+        return lineWidth;
+    }
+    
+    public void setLineWidth(int l){
+        lineWidth = l;
     }
     
 }
