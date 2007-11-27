25d43ff7853820a4551250ed7d6a413017b1c16a
karl <karl@7f32de98-0a3d-0410-aeab-8638215ba1a0>
2007-11-27 14:08:57 +0000

    Swing Gui Work

--- Alchemy/src/alchemy/ui/AlcUi.java
+++ Alchemy/src/alchemy/ui/AlcUi.java
@@ -14,8 +14,6 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
-
 public class AlcUi extends JPanel implements ActionListener { // Extend JPanel rather than JComponent so the background can be set
     
     /** Reference to the root **/
@@ -23,9 +21,13 @@ public class AlcUi extends JPanel implements ActionListener { // Extend JPanel r
     /** Visibility of the Ui */
     private boolean visible = true;
     /** Height of the Ui */
-    private int uiHeight = 50;
+    private int uiHeight = 65;
     /** Ui Background Colour */
     private static Color uiBgColour = new Color(230, 230, 230);
+    /** Ui Text Size */
+    private static final int uiTextSize = 10;
+    /** Ui Popup Menu Y Location */
+    private static final int uiPopupMenuY = 55;
     
     /** Combo Box drop down menu with the list of create functions */
     JComboBox createComboBox;
@@ -36,23 +38,74 @@ public class AlcUi extends JPanel implements ActionListener { // Extend JPanel r
     /** Action command for 'affects' */
     private static String AFFECT_COMMAND = "affect";
     
+    private JPopupMenu createPopup;
+    
     /** Creates a new instance of AlcUi */
     public AlcUi(AlcMain root, Dimension windowSize, String[] createNames, String[] affectNames) {
+        
+        // General Toolbar settings
         this.root = root;
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        JButton mybtn = new JButton("Do Something");
-        
-        this.add(mybtn);
         this.setBackground(uiBgColour);
+        setUiVisible(false);
+        this.setLocation(0, 0);
+        this.setBounds(0, 0, windowSize.width, uiHeight);
         
+        // Buttons
+        AlcButton markButton = new AlcButton(this, "Marks");
+        this.add(markButton);
+        
+        AlcButton createButton = new AlcButton(this, "Create");
+        this.add(createButton);
+        
+        // PopupMenus on the buttons
         if(createNames != null){
-            createComboBox = new JComboBox(createNames);
-            //createComboBox.setSelectedIndex(2);
+            createPopup = new AlcPopupMenu(this);
+            
+            // Populate the Popup Menu
+            for (int i = 0; i < createNames.length; i++) {
+                JMenuItem menuItem = new JMenuItem(createNames[i]);
+                menuItem.setActionCommand(CREATE_COMMAND);
+                menuItem.addActionListener(this);
+                createPopup.add(menuItem);
+            }
+        }
+        
+        /*
+        //JMenuItem item;
+        popup.add(new JMenuItem("Cut"));
+        popup.addSeparator();
+        popup.add(new JMenuItem("Copy"));
+        popup.add(new JMenuItem("Copy"));
+        popup.add(new JMenuItem("Copy"));
+        popup.add(new JMenuItem("Copy"));
+         
+         */
+        //popup.setBorderPainted(false);
+        //popup.setInvoker(mybtn);
+        
+        createButton.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                createPopup.show(e.getComponent(), 0, uiPopupMenuY);
+            }
+        });
+        
+        //JToggleButton tbtn = new JToggleButton("Yes");
+        //this.add(tbtn);
+        
+        
+        /*
+        if(createNames != null){
+         
+            createComboBox = new AlcComboBox(this, createNames);
+         
             createComboBox.setActionCommand(CREATE_COMMAND);
             createComboBox.addActionListener(this);
             this.add(createComboBox);
         }
+         */
         
+        /*
         if(affectNames != null){
             affectComboBox = new JComboBox(affectNames);
             //affectComboBox.setSelectedIndex(2);
@@ -60,10 +113,9 @@ public class AlcUi extends JPanel implements ActionListener { // Extend JPanel r
             //affectComboBox.addActionListener(this);
             this.add(affectComboBox);
         }
+         */
         
-        setUiVisible(false);
-        this.setLocation(0, 0);
-        this.setBounds(0, 0, windowSize.width, uiHeight);
+        
         
     }
     
@@ -86,15 +138,32 @@ public class AlcUi extends JPanel implements ActionListener { // Extend JPanel r
             if(visible) {
                 this.setVisible(false);
                 //System.out.println("OFF");
+                // Turn off the popupMenu too!
+                if(createPopup != null) createPopup.setVisible(false);
                 visible = false;
             }
         }
         
     }
     
+    
+    // GETTERS
+    /** Return the height of the UI Toolbar */
     public int getUiHeight(){
         return uiHeight;
     }
+    
+    /** Return the colour of the UI toolbar background */
+    public Color getUiBgColour(){
+        return uiBgColour;
+    }
+    
+    /** Return the height of the UI Toolbar */
+    public int getUiTextSize(){
+        return uiTextSize;
+    }
+    
+    
     
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
