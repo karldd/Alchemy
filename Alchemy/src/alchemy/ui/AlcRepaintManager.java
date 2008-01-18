5625f13ebc261aae64fa0feff4fe5d976b5acc33
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2008-01-18 03:02:23 +0000

    Repaint manger changes - Icons updated

--- Alchemy/src/alchemy/ui/AlcRepaintManager.java
+++ Alchemy/src/alchemy/ui/AlcRepaintManager.java
@@ -5,21 +5,26 @@ import javax.swing.JComponent;
 import java.awt.Container;
 
 public class AlcRepaintManager extends RepaintManager {
+
     public void addDirtyRegion(JComponent comp, int x, int y, int w, int h) {
-        super.addDirtyRegion(comp,x,y,w,h);
+        super.addDirtyRegion(comp, x, y, w, h);
         JComponent root = getRootJComponent(comp);
         // to avoid a recursive infinite loop
-        if(comp != root) {
-            super.addDirtyRegion(root,0,0,root.getWidth(),root.getHeight());
+
+        if (comp != root) {
+            //System.out.println(root.getClass());
+            if (!root.getClass().getName().endsWith("JRootPane")) {
+                super.addDirtyRegion(root, 0, 0, root.getWidth(), root.getHeight());
+            }
         }
     }
+
     public JComponent getRootJComponent(JComponent comp) {
         Container parent = comp.getParent();
-        if(parent instanceof JComponent) {
-            return getRootJComponent((JComponent)parent);
+        if (parent instanceof JComponent) {
+            return getRootJComponent((JComponent) parent);
         }
         return comp;
     }
-
 }
 
