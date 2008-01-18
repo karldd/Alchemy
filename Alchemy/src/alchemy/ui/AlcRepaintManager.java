e6b939757cc09e7d0537d0b9507e4dd827c4d3eb
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2008-01-18 03:15:59 +0000

    Repaint manager changes - Redraw AlcMenu parent only

--- Alchemy/src/alchemy/ui/AlcRepaintManager.java
+++ Alchemy/src/alchemy/ui/AlcRepaintManager.java
@@ -1,30 +1,39 @@
 package alchemy.ui;
 
+import java.awt.Container;
 import javax.swing.RepaintManager;
 import javax.swing.JComponent;
-import java.awt.Container;
 
 public class AlcRepaintManager extends RepaintManager {
 
     public void addDirtyRegion(JComponent comp, int x, int y, int w, int h) {
         super.addDirtyRegion(comp, x, y, w, h);
-        JComponent root = getRootJComponent(comp);
-        // to avoid a recursive infinite loop
-
-        if (comp != root) {
-            //System.out.println(root.getClass());
-            if (!root.getClass().getName().endsWith("JRootPane")) {
-                super.addDirtyRegion(root, 0, 0, root.getWidth(), root.getHeight());
+        // Make sure that the semi transparent AlcMenu is redrawn properly
+        if (comp.getClass().getName().endsWith("AlcMenu")) {
+            Container parentContainer = comp.getParent();
+            if (parentContainer instanceof JComponent) {
+                JComponent parent = (JComponent) parentContainer;
+                super.addDirtyRegion(parent, 0, 0, parent.getWidth(), parent.getHeight());
             }
         }
+    //JComponent root = getRootJComponent(comp);
+    // to avoid a recursive infinite loop
+
+    //System.out.println(comp);
+//        if (comp != root) {
+//            //System.out.println(root.getClass());
+//            super.addDirtyRegion(root, 0, 0, root.getWidth(), root.getHeight());
+//
+//        }
     }
 
-    public JComponent getRootJComponent(JComponent comp) {
-        Container parent = comp.getParent();
-        if (parent instanceof JComponent) {
-            return getRootJComponent((JComponent) parent);
-        }
-        return comp;
-    }
+//    public JComponent getRootJComponent(JComponent comp) {
+//        Container parent = comp.getParent();
+//        if (parent instanceof JComponent) {
+//            //return getRootJComponent((JComponent) parent);
+//            return (JComponent) parent;
+//        }
+//        return comp;
+//    }
 }
 
