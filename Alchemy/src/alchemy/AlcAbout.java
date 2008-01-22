61622bdea0319f9233929cbb01de184eb8e744c6
karl <karl@7f32de98-0a3d-0410-aeab-8638215ba1a0>
2008-01-22 02:14:12 +0000

    About screen changed to its own green coloured class & updated

--- Alchemy/src/alchemy/AlcAbout.java
+++ Alchemy/src/alchemy/AlcAbout.java
@@ -1,64 +1,70 @@
 /*
- *  This file is part of the Alchemy project - http://al.chemy.org
+ * This file is part of the Alchemy project - http://al.chemy.org
  * 
- *  Copyright (c) 2007 Karl D.D. Willis
+ * Copyright (c) 2007 Karl D.D. Willis
  * 
- *  Alchemy is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Alchemy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- *  Alchemy is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Alchemy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- *  You should have received a copy of the GNU General Public License
- *  along with Alchemy.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * You should have received a copy of the GNU General Public License
+ * along with Alchemy.  If not, see <http://www.gnu.org/licenses/>.
  */
 package alchemy;
 
 import alchemy.ui.AlcToolBar;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /**
- *  About Window 
+ * The Alchemy about window
  */
-public class AlcAbout extends JDialog implements ActionListener {
+public class AlcAbout extends Window implements AlcConstants {
 
-    private final JPanel aboutPanel;
+    private final Image image;
+    private final Dimension size;
 
-    public AlcAbout(AlcMain root, String title) {
+    public AlcAbout(Window owner) {
+        super(owner);
 
-        super(root, title);
-        aboutPanel = new JPanel();
-        aboutPanel.setBackground(AlcToolBar.toolBarBgColour);
+        AlcMain root = (AlcMain) owner;
 
-        JButton closeButton = new JButton("Close");
-        closeButton.setFocusPainted(false);
-        closeButton.setOpaque(false);
-        closeButton.addActionListener(new ActionListener() {
+        image = AlcUtil.getImage("data/about.png", root);
+        size = new Dimension(image.getWidth(root), image.getHeight(root));
 
-            public void actionPerformed(ActionEvent e) {
+        this.addMouseListener(new MouseAdapter() {
+
+            public void mousePressed(MouseEvent e) {
                 setVisible(false);
                 dispose();
             }
         });
-        aboutPanel.add(closeButton);
-        aboutPanel.setPreferredSize(new Dimension(400, 300));
 
-        this.setContentPane(aboutPanel);
-        this.setResizable(false);
-        this.pack();
-        this.setLocationRelativeTo(root);
+        this.setSize(size);
+        Point loc = AlcUtil.calculateCenter(this);
+        this.setBounds(loc.x, loc.y, size.width, size.height);
         this.setVisible(true);
 
     }
 
-    public void actionPerformed(ActionEvent arg0) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void paint(Graphics g) {
+        int x = size.width / 2;
+        
+        g.drawImage(image, 0, 0, null);
+
+        //Graphics2D g2 = (Graphics2D) g;
+        //g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+
+        g.setFont(AlcToolBar.subToolBarFont);
+        g.setColor(Color.white);
+        g.drawString(ALCHEMY_VERSION, 360, 277);
+
     }
 }
