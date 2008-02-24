0b97b021f1e5c47aa971f4a1e4b5403a54a1aad1
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2008-02-24 02:59:55 +0000

    Leopard menubar issue resolved

--- Alchemy/src/alchemy/AlcMain.java
+++ Alchemy/src/alchemy/AlcMain.java
@@ -221,19 +221,11 @@ public class AlcMain extends JFrame implements AlcConstants, ComponentListener, 
         menuBar = new AlcMenuBar(this);
 
         if (PLATFORM == MACOSX) {
-
-
-            // For some reason the menubar still displays on OSX
-            // Set it to invisible for Leopard?
-            //menuBar.setVisible(false);
-            menuBar.setOpaque(false);
-            menuBar.setBorderPainted(false);
             // Add normally if on MacOSX as the menu is listed above
             this.setJMenuBar(menuBar);
 
         } else {
             // Otherwise add it to the toolbar area
-
             //Palette
             if (prefs.getPaletteAttached()) {
                 this.setJMenuBar(menuBar);
