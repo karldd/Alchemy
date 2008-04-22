1a62046c29b2aebf5a842c5997995ab54be3c44f
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2008-04-22 09:25:31 +0000

    DetachShapes / ScrawlShapes added

---
+++ Alchemy/src/org/alchemy/create/DetachShapes.java
@@ -0,0 +1,134 @@
+/*
+ * This file is part of the Alchemy project - http://al.chemy.org
+ * 
+ * Copyright (c) 2007 Karl D.D. Willis
+ * 
+ * Alchemy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alchemy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Alchemy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alchemy.create;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import org.alchemy.core.*;
+
+/**
+ * DetachShapes
+ * @author Karl D.D. Willis
+ */
+public class DetachShapes extends AlcModule {
+
+    private double x = 100;
+    private double y = 100;
+    private double angle1 = 0;
+    private int distance = 100;
+    //private Point oldP;
+    private boolean newPath;
+    private AlcSubToolBarSection subToolBarSection;
+
+    public DetachShapes() {
+
+    }
+
+    @Override
+    protected void setup() {
+        createSubToolBarSection();
+        toolBar.addSubToolBarSection(subToolBarSection);
+    }
+
+    @Override
+    protected void reselect() {
+        // Add this modules toolbar to the main ui
+        toolBar.addSubToolBarSection(subToolBarSection);
+    }
+
+    public void createSubToolBarSection() {
+
+        subToolBarSection = new AlcSubToolBarSection(this);
+
+        // Tolerance Slider
+        AlcSubSlider distanceSlider = new AlcSubSlider("Distance", 0, 300, distance);
+        distanceSlider.setToolTipText("Adjust the drawing distance");
+        distanceSlider.slider.addChangeListener(
+                new ChangeListener() {
+
+                    public void stateChanged(ChangeEvent e) {
+                        JSlider source = (JSlider) e.getSource();
+                        if (!source.getValueIsAdjusting()) {
+                            distance = source.getValue();
+
+                        }
+                    }
+                });
+        subToolBarSection.add(distanceSlider);
+    }
+
+//    private static int getCursorSpeed(Point p1, Point p2) {
+//        int diffX = Math.abs(p1.x - p2.x);
+//        int diffY = Math.abs(p1.y - p2.y);
+//        return diffX + diffY;
+//    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+//        Point p = e.getPoint();
+//        canvas.createShapes.add(new AlcShape(p));
+//        canvas.redraw();
+        newPath = true;
+        //oldP = e.getPoint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Point p = e.getPoint();
+        //int speed = 100 - (getCursorSpeed(p, oldP));
+        //int speed = 100;
+        //System.out.println(speed);
+
+        double dx = p.x - x;
+        double dy = p.y - y;
+        angle1 = Math.atan2(dy, dx);
+        x = p.x - (Math.cos(angle1) * distance);
+        y = p.y - (Math.sin(angle1) * distance);
+
+        Point newPt = new Point((int) x, (int) y);
+
+        if (newPath) {
+            canvas.createShapes.add(new AlcShape(newPt));
+            canvas.redraw();
+            //oldP = p;
+            newPath = false;
+        } else {
+            if (canvas.getCurrentCreateShape() != null) {
+                canvas.getCurrentCreateShape().addCurvePoint(newPt);
+                canvas.redraw();
+                //oldP = p;
+            }
+        }
+//        double xx = x + (segLength * Math.cos(angle1));
+//        double yy = y + (segLength * Math.sin(angle1));
+
+    //segment(x, y, angle1);
+    //ellipse(x, y, 20, 20);
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        canvas.redraw();
+        canvas.commitShapes();
+        //oldP = null;
+    }
+}
