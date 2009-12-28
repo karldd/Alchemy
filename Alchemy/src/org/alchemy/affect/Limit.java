e570814d4730efad818075b848fb1bca5b342fb5
Karl D.D. Willis <619336+karldd@users.noreply.github.com>
2009-12-28 01:35:53 +0000

    Limit debugged Ribbon shapes tweaked

--- Alchemy/src/org/alchemy/affect/Limit.java
+++ Alchemy/src/org/alchemy/affect/Limit.java
@@ -26,10 +26,8 @@ import org.alchemy.core.*;
 public class Limit extends AlcModule {
 
     private int defaultNumber = 10;
-    private int limit = defaultNumber - 1;
-
+    private int limit = defaultNumber;
     private AlcToolBarSubSection subToolBarSection;
-
 
     @Override
     public void setup() {
@@ -53,23 +51,49 @@ public class Limit extends AlcModule {
                     public void stateChanged(ChangeEvent e) {
                         if (!numberSpinner.getValueIsAdjusting()) {
                             int value = numberSpinner.getValue();
-                            limit = value - 1;
+                            limit = value;
                         }
                     }
                 });
         subToolBarSection.add(numberSpinner);
     }
 
-
     @Override
     protected void affect() {
 
-        if(canvas.shapes.size() > limit){
+        // We only limit the main shapes list
+        // so if that is empty then return
+        if(canvas.shapes.size() == 0){
+            return;
+        }
 
-            int end = canvas.shapes.size() - limit;
-            canvas.shapes.subList(0, end).clear();
+        // The total number of shapes
+        int totalShapes = canvas.shapes.size() + canvas.createShapes.size() + canvas.affectShapes.size();
+
+        // If there are more shapes than the limit
+        if (totalShapes > limit) {
+
+//            System.out.println("Shapes: " + canvas.shapes.size() + " Create: " + canvas.createShapes.size() + " Affect: " + canvas.affectShapes.size());
+
+            // The number of shapes to remove
+            int shapesToRemove = totalShapes - limit;
+//            System.out.println("Total: " + totalShapes + " Remove: " + shapesToRemove);
+
+            // The end of the range to remove from the shapes list
+            int end = shapesToRemove;
+            // If there are not enough shapes in the shapes list
+            if (shapesToRemove > canvas.shapes.size()) {
+                end = canvas.shapes.size() - 1;
+            }
+            if(end == 0){
+                canvas.shapes.remove(0);
+                shapesToRemove--;
+            } else if (end > 0) {
+                canvas.shapes.subList(0, end).clear();
+                shapesToRemove -= end + 1;
+            }
+
             canvas.redraw(true);
-
         }
 
     }
